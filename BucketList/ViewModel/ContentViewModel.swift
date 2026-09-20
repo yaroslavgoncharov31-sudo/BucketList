e185481605eec6642558ac0427d49397343561a9
roundeeesss <yaroslavgoncharov31@gmail.com>
2026-09-20 14:24:58 +0200

    add example MapLocation to use in previews

---
+++ BucketList/ViewModel/ContentViewModel.swift
@@ -0,0 +1,19 @@
+import MapKit
+import Foundation
+import SwiftUI
+
+@Observable
+class ContentViewModel {
+    let position = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 56, longitude: -3),
+            span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
+            )
+    )
+    var locations = [MapLocation]()
+    var selectedLocation: MapLocation?
+
+    #if DEBUG
+    static let example = MapLocation(id: UUID(), name: "Buckingham Palace", description: "Lit by over 40,000 lightbulbs.", latitude: 51.501, longitude: -0.141)
+    #endif
+}
