110b10057a141966817326a7592bc3d0174b765a
roundeeesss <yaroslavgoncharov31@gmail.com>
2026-09-25 12:29:58 +0200

    add styleOption and loadingStates enums

---
+++ BucketList/Model/LoadingStates.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+enum LoadingStates {
+    case loading
+    case loaded
+    case failed
+}
---
+++ BucketList/Model/StyleOption.swift
@@ -0,0 +1,9 @@
+import Foundation
+import MapKit
+import SwiftUI
+
+enum MapStyleOption {
+    case standard
+    case hybrid
+    case imagery
+}
